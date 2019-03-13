54476d8d822f117f26dc0be37529596168d798f7
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-03-13 17:36:14 -0400

    update

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # carbon
 [image-tile-server](https://github.com/hfu/image-tile-server) to be integrated with [onyx](https://github.com/un-vector-tile-toolkit/onyx)
 
+# limitations: Works with Node 8 only
+As discussed [here](https://github.com/mapbox/mapbox-gl-native/issues/12252), node-mapbox-gl-native has some issue with Node 10. Please use Node 8. See also `.node-version`.
+
 # install
 ```console
 git clone git@github.com:hfu/carbon
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const config = require('config')
 const express = require('express')
 const mbgl = require('@mapbox/mapbox-gl-native')
-const tilebelt = require('@mapbox/tilebelt')
+// const tilebelt = require('@mapbox/tilebelt')
 const vtpbf = require('vt-pbf')
-const request = require('request')
+// const request = require('request')
 const sharp = require('sharp')
 const fs = require('fs')
 const Queue = require('better-queue')
 const MBTiles = require('@mapbox/mbtiles')
 const zlib = require('zlib')
+const genericPool = require('generic-pool')
 
 const htdocsPath = config.get('htdocsPath')
 const stylePath = config.get('stylePath')
@@ -38,9 +39,6 @@ const etag = 'a'
 let mbtilesPool = {}
 let tz = config.get('tz')
 
-const app = express()
-app.use(express.static(htdocsPath))
-
 const createResponse = data => {
   return {
     modified: new Date(),
@@ -61,7 +59,7 @@ const getMBTiles = async (t, z, x, y) => {
       if (fs.existsSync(mbtilesPath)) {
         new MBTiles(`${mbtilesPath}?mode=ro`, (err, mbtiles) => {
           if (err) return reject(err)
-          console.log(`${mbtilesPath} was newly open.`)
+          // console.log(`${mbtilesPath} was newly open.`)
           mbtilesPool[mbtilesPath] = mbtiles
           resolve(mbtiles)
         })
@@ -80,17 +78,17 @@ const getTile = (url) => {
     const x = parseInt(r[r.length - 2])
     const y = parseInt(r[r.length - 1].split('.')[0])
     getMBTiles(t, z, x, y)
-    .then(mbtiles => {
-      mbtiles.getTile(z, x, y, (err, tile, headers) => {
-        if (err) {
-          resolve(emptyTile)
-        } else {
-          resolve(zlib.gunzipSync(tile))
-        }
+      .then(mbtiles => {
+        mbtiles.getTile(z, x, y, (err, tile, headers) => {
+          if (err) {
+            resolve(emptyTile)
+          } else {
+            resolve(zlib.gunzipSync(tile))
+          }
+        })
+      }).catch(e => {
+        resolve(emptyTile)
       })
-    }).catch(e => {
-      resolve(emptyTile)
-    })
   })
 }
 
@@ -117,8 +115,7 @@ const getFont = async (url) => {
 }
 
 const mbglRequestQueue = new Queue((req, cb) => {
-  console.log(req.url)
-  switch(req.kind) {
+  switch (req.kind) {
     case 6:
       fs.readFile(spriteJsonPath, (err, data) => {
         if (err) cb(err)
@@ -143,51 +140,72 @@ const mbglRequestQueue = new Queue((req, cb) => {
       break
     default:
   }
+}, { concurrent: 6 })
+
+const maps = genericPool.createPool({
+  create: function () {
+    const map = new mbgl.Map({
+      request: (req, cb) => {
+        mbglRequestQueue.push(req, (err, data) => {
+          if (err) cb(err)
+          cb(null, createResponse(data))
+        })
+      },
+      mode: 'tile',
+    })
+    map.load(require(stylePath))
+    return map
+  },
+  destroy: function (map) {
+    map.release()
+  }
+}, {
+  max: 10,
+  min: 2
 })
 
 const tileQueue = new Queue((r, cb) => {
   const [t, z, x, y] = [r.t, r.z, r.x, r.y]
   const center = [ tile2long(x + 0.5, z), tile2lat(y + 0.5, z) ]
 
-  const map = new mbgl.Map({
-    request: (req, cb) => {
-      mbglRequestQueue.push(req, (err, data) => {
-        if (err) cb(err)
-        cb(null, createResponse(data))
+  maps.acquire().then(map => {
+    map.render({
+      zoom: z,
+      center: center,
+      width: z > 2 ? 1024 : 512,
+      height: z > 2 ? 1024 : 512
+      //width: 512,
+      //height: 512
+    }, (err, buffer) => {
+      maps.release(map)
+      if (err) return cb(err)
+      let image = sharp(buffer, {
+        raw: {
+          width: z > 2 ? 1024 : 512,
+          height: z > 2 ? 1024 : 512,
+          //width: 512,
+          //height: 512,
+          channels: 4
+        }
       })
-    }
-  })
-
-  map.load(require(stylePath))
-  map.render({
-    zoom: z,
-    center: center,
-    width: z > 2 ? 1024 : 512,
-    height: z > 2 ? 1024 : 512
-  }, (err, buffer) => {
-    map.release()
-    if (err) return cb(err)
-    let image = sharp(buffer, {
-      raw: {
-        width: z > 2 ? 1024 : 512,
-        height: z > 2 ? 1024: 512,
-        channels: 4
+      if (z > 2) {
+        image = image.extract({
+          left: 256, top: 256, width: 512, height: 512
+        })
       }
+      cb(null, image)
     })
-    if (z > 2) {
-      image = image.extract({
-        left: 256, top: 256, width: 512, height: 512
-      })
-    }
-    cb(null, image)
   })
-})
+}, { concurrent: 6 })
+
+const app = express()
+app.use(express.static(htdocsPath))
 
 app.get('/:t/:z/:x/:y.png', (req, res) => {
   tileQueue.push({
     t: req.params.t,
     z: parseInt(req.params.z),
-    x: parseInt(req.params.x), 
+    x: parseInt(req.params.x),
     y: parseInt(req.params.y)
   }, (err, image) => {
     if (err) {
@@ -195,9 +213,9 @@ app.get('/:t/:z/:x/:y.png', (req, res) => {
     } else {
       res.set('content-type', 'image/png')
       image.png().toBuffer()
-      .then((result) => {
-        res.send(result)
-      })
+        .then((result) => {
+          res.send(result)
+        })
     }
   })
 })
